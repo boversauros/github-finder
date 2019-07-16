34c5f9eec1496eab5644c18d4ffaa83f8947d882
Oriol <bover73@gmail.com>
2019-07-16 20:21:54 +0200

    add github functionalities

--- src/github-api/index.js
+++ src/github-api/index.js
@@ -4,8 +4,29 @@ const axios = require('axios')
 const githubApi = {
     url: 'https://api.github.com',
 
-    searchUsers(text) {
-        axios.get(`${this.url}/search/users?q=${text}`)
+    searchUsers(query) {
+        axios.get(`${this.url}/search/users?q=${query}`)
+            .then(res => {
+                console.log(res)
+            })
+    },
+
+    searchRepos(query) {
+        axios.get(`${this.url}/search/repositories?q=${query}`)
+            .then(res => {
+                console.log(res)
+            })
+    },
+
+    getUser(username) {
+        axios.get(`${this.url}/users/${username}`)
+            .then(res => {
+                console.log(res)
+            })
+    },
+
+    getUserRepos(username) {
+        axios.get(`${this.url}/users/${username}/repos`)
             .then(res => {
                 console.log(res)
             })
